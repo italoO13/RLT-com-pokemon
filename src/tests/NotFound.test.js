7750edc953539cc3e09d7b2f9c6585253389c408
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-05-09 15:40:10 -0300

    Requisito 4 ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testes para o requisito 4', () => {
+  test('Verifica se o h2 da pag contem Page request...', () => {
+    renderWithRouter(<NotFound />);
+    const titleNotFoud = screen
+      .getByRole('heading', { name: /Page requested not found/i }, { level: 2 });
+    expect(titleNotFoud).toBeInTheDocument();
+    const img = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
