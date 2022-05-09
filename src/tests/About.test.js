0fa3a260dd137f1e6dc8e96bde788e5f51993a57
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-05-09 15:27:28 -0300

    Requisito 2 ok

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testes para o requisito 2', () => {
+  test('testa se contem info sobre pokedex', () => {
+    renderWithRouter(<About />);
+    const titleAbout = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(titleAbout).toBeInTheDocument();
+  });
+  test('Teste se contem dois paragrafos no About', () => {
+    renderWithRouter(<About />);
+    const paragrafo1 = screen.getByText(/This application simulates a Pokédex/i);
+    expect(paragrafo1).toBeInTheDocument();
+    const paragrafo2 = screen.getByText(/One can filter Pokémons by/i);
+    expect(paragrafo2).toBeInTheDocument();
+  });
+  test('Testa se imagem com src especifico renderizou na tela', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByRole('img', { name: 'Pokédex' });
+    expect(img)
+      .toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
